d5abb3c559f400416d667008a19e6c6cb41aea78
DESKTOP-C59N7GN\Valera <lappo.valera@yandex.ru>
2022-11-19 21:20:40 +0300

    First video: setup database and domain entities

---
+++ SimpleTrader/SimpleTrader.Domain/Services/IDataService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleTrader.Domain.Services
+{
+    public interface IDataService<T>
+    {
+        Task<IEnumerable<T>> GetAll();
+        Task<T> GetId(int id);
+
+        Task<T> Create(T entity);
+
+        Task<T> Update(int id, T newValue);
+
+        Task<bool> Delete(int id);
+
+    }
+}
---
+++ SimpleTrader/SimpleTrader.EntityFramework/SimpleTraderDbContext.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.EntityFrameworkCore;
+using SimpleTrader.Domain.Models;
+
+namespace SimpleTrader.EntityFramework
+{
+    public class SimpleTraderDbContext : DbContext
+    {
+        //This class is going to manage out inner action
+        //with the database with Entity Framework
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<Account> Accounts { get; set; }
+
+        public DbSet<AssetTransaction> AssetTransactions { get; set; }
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            //tell database that owr stock will not be an entity and will be embedded in AssetTransAction
+            modelBuilder.Entity<AssetTransaction>().OwnsOne(a => a.Stock);
+
+            base.OnModelCreating(modelBuilder);
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            //this method only available with sqlserver nuget package
+            optionsBuilder.UseSqlServer("Server=(localdb)\\MSSQLLocalDB;Database=SimpleTraderDB;Trusted_Connection=True");
+
+            base.OnConfiguring(optionsBuilder);
+        }
+    }
+}
